222d0a0003d82693f0f016c48848ac43e06aa8d7
rajaraja123 <Rajapranatapanjaitan@gmail.com>
2022-11-14 20:58:33 +0700

    updated fix

--- angular_learning/src/app/user-management/user-creation/user-creation.component.ts
+++ angular_learning/src/app/user-management/user-creation/user-creation.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, AfterContentChecked } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserEditComponent } from '../user-edit/user-edit.component';
 import { UserService } from '../user.service';
+import Swal from 'sweetalert2';
 export interface List {id : string ,name : string ,email: string, age : number , gender : string , position :string, marital : string , addresgrup : addres[]} 
 export interface addres {addres :string , zip : string ,  city : string , country : string}
 
@@ -22,13 +22,14 @@ export class UserCreationComponent implements OnInit {
   private listid : string|null =null
   public list : List|null = null
   public form : FormGroup 
+  value: any;
 
   constructor(private fb : FormBuilder ,private router : Router, private Service : UserService ) { 
     
     this.form = this.fb.group({
-      id : [null ,[Validators.required]],
-      name : [null ,[Validators.required]],
-      age : [null ,[Validators.required,Validators.min(19)]],
+      id : [null ,[Validators.required,Validators.pattern("^[0-9]*$")]],
+      name : [null ,[Validators.required,Validators.pattern("^[a-z,A-Z ]*$")]],
+      age : [null ,[Validators.required,Validators.min(10),Validators.pattern("^[0-9]*$")]],
       gender : [null ,[Validators.required]],
       email : [null ,[Validators.required , Validators.email]],
       position : [null ,[Validators.required]],
@@ -40,7 +41,13 @@ export class UserCreationComponent implements OnInit {
 
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
+  cek : any;
   ngOnInit(): void {
+    this.form.statusChanges.subscribe((value ) => {
+      this.cek = value     
+      console.log(this.cek)
+    })
+    this.value()
   }
 
   poss: pos[] = [
@@ -55,18 +62,32 @@ export class UserCreationComponent implements OnInit {
   
   adressformgrup() : FormGroup {
     return this.fb.group({
-      addres : [null ,[Validators.required]],
-      zip : [null ,[Validators.required,Validators.minLength(6),Validators.maxLength(9)]],
+      addres : [null ,[Validators.required ,]],
+      zip : [null ,[Validators.required,Validators.minLength(6),Validators.maxLength(9) ,Validators.pattern("^[0-9]*$")]],
       city : [null ,[Validators.required]],
       country : [null ,[Validators.required]],
     })
   }
 
   submit() : void {
-    const payload = this.form.value;
-    console.log(payload);
-    this.Service.addUser(payload)
-    this.router.navigate(['user-management','list'])
+    if (this.cek == "INVALID") {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Data tidak Valid'
+      })
+    } else {
+      Swal.fire(
+        'Good job!',
+        'success'
+      )
+      setTimeout(() => {
+        const payload = this.form.value;
+        console.log(payload);
+        this.Service.addUser(payload)
+        this.router.navigate(['..','list'])
+      }, 1000)
+    }
     
     
   }
